680c8480e9e7f986c65947d2c8909947df2bba12
gunalhakan <gunalhakan@gmail.com>
2025-07-24 16:18:19 +0300

    model directive examples added

--- Basics/Controllers/EmployeeController.cs
+++ Basics/Controllers/EmployeeController.cs
@@ -1,22 +1,39 @@
+
+using Basics.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Basics.Controllers
 {
     public class EmployeeController : Controller
     {
-        public String Index()
+        public IActionResult Index1()
         {
-            return "Hello World!!!";
+            string message = $"asdfad->{DateTime.Now.ToString()}";
+            return View("Index1",message);
         }
 
         public ViewResult Index2()
         {
-            return View("Index");
+            var names = new String[]
+            {
+                "Güney",
+                "Kuzey",
+                "Doğu",
+                "Batı"
+            };
+
+            return View(names);
         }
 
         public IActionResult Index3()
         {
-            return Content("Employeee");
+            var list = new List<Employee>
+            { 
+                new Employee(){Id=1,FirstName="Ekin",LastName="Cömer",Age=26},
+                new Employee(){Id=2,FirstName="Mert",LastName="Semin",Age=26},
+                new Employee(){Id=3,FirstName="İpek",LastName="Demir",Age=26}
+            };
+            return View("Index3",list);
         }
        
     }
