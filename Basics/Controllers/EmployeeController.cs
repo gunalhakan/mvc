a1a49c6f8f0229798c151cf86734db93248084f1
gunalhakan <gunalhakan@gmail.com>
2025-07-24 15:12:38 +0300

    html rendering examples added

---
+++ Basics/Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basics.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public String Index()
+        {
+            return "Hello World!!!";
+        }
+
+        public ViewResult Index2()
+        {
+            return View("Index");
+        }
+
+        public IActionResult Index3()
+        {
+            return Content("Employeee");
+        }
+       
+    }
+
+}
