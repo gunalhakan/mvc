82adad69ce87b6b734dc3d65f5a975089b00eeaa
gunalhakan <gunalhakan@gmail.com>
2025-07-25 12:53:01 +0300

    define candidate model add operation added

---
+++ CourseRegister/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace courseRegister.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
