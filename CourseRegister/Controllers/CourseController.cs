f2ce19a93d8c448a096a7ea98ea1c11d1957efa0
gunalhakan <gunalhakan@gmail.com>
2025-09-07 15:19:53 +0300

    CourseRegiter application finished

---
+++ CourseRegister/Controllers/CourseController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using CourseRegister.Models;
+
+namespace CourseRegister.Controllers
+{
+    public class CourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = Repository.Applications;
+            return View(model);
+        }
+        public IActionResult Apply()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Apply([FromForm] Candidate model)
+        {
+            if (Repository.Applications.Any(c => c.Email.Equals(model.Email)))
+            {
+                ModelState.AddModelError("", "There is already an application");
+            }
+            if (ModelState.IsValid)
+            {
+
+                Repository.Add(model);
+                return View("Feedback", model);
+            }
+            return View();
+        }
+    }
+}
